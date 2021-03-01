2d9645243b4aea710654d59e05357f7bc498af8e
IluhaTheSlayer <lemon8492@gmail.com>
2021-03-01 17:46:55 +0300

    GC Interaction

---
+++ GameCoordinator.cs
@@ -0,0 +1,40 @@
+using SteamKit2;
+using System;
+using SteamKit2.GC;
+using SteamKit2.GC.CSGO.Internal;
+
+namespace GameCoordinator
+
+        void OnGCMessage(SteamGameCoordinator.MessageCallback callback)
+        {
+            if (callback == null)
+                return;
+
+            Console.WriteLine(string.Format("OnGCMessage code : {0}", callback.EMsg));
+
+            var messageMap = new Dictionary<uint, Action<IPacketGCMsg>>
+            {
+                { ( uint )EGCBaseClientMsg.k_EMsgGCClientWelcome, OnClientWelcome },
+                { ( uint )ECsgoGCMsg.k_EMsgGCCStrike15_v2_MatchmakingGC2ClientHello, OnMatchmakingClientHello },
+                { (uint)EMsg.ClientMMSJoinLobbyResponse, OnJoinLobbyResponse },
+                { (uint)EMsg.ClientMMSCreateLobbyResponse, OnCreateLobbyResponse }
+        	};
+          
+   async void OnClientWelcome(IPacketGCMsg packetMsg)
+        {
+            var msg = new ClientGCMsgProtobuf<CMsgClientWelcome>(packetMsg);
+
+            form.AddLog(String.Format("MM welcomes us. Your current location: {0}", msg.Body.location.country));
+
+            var getUser = new ClientGCMsgProtobuf<CMsgStoreGetUserData>((uint)EGCItemMsg.k_EMsgGCStoreGetUserData);
+            getUser.Body.currency = 2;
+            GameCoordinator.Send(storeGetUser, 730);
+
+            var requestCoPlays = new ClientGCMsgProtobuf<CMsgGCCStrike15_v2_Account_RequestCoPlays>((uint)ECsgoGCMsg.k_EMsgGCCStrike15_v2_Account_RequestCoPlays);
+            GameCoordinator.Send(requestCoPlays, 730);
+
+            GameCoordinator.Send(getUser, 730);
+
+            form.ToggleConnected(true);
+            await Task.Delay(TimeSpan.FromSeconds(2)).ConfigureAwait(false);
+        }
