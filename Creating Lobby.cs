befcac87e95c57101a768dc5ad4d17ab03580143
IluhaTheSlayer <lemon8492@gmail.com>
2021-03-01 17:51:43 +0300

    Update Creating Lobby.cs

--- Creating Lobby.cs
+++ Creating Lobby.cs
@@ -3,18 +3,18 @@ using System;
 using SteamKit2.GC;
 using SteamKit2.GC.CSGO.Internal;
 
-        public void CreateLobby(string _username)
+        public void CreateLobby()
         {
-            var createRequest = new ClientMsgProtobuf<CMsgClientMMSCreateLobby>(EMsg.ClientMMSCreateLobby);
-            createRequest.Header.Proto.routing_appid = 730; // CS:GO APPID
-            createRequest.ProtoHeader.routing_appid = 730;
-            createRequest.Body.app_id = 730;
-            createRequest.Body.max_members = 10;
-            createRequest.Body.lobby_type = 1;
-            createRequest.Body.lobby_flags = 1;
-            createRequest.Body.cell_id = cellId;
-            createRequest.Body.persona_name_owner = Friends.GetPersonaName();
-            steamClient.Send(createRequest);
+            var createlobbyRequest = new ClientMsgProtobuf<CMsgClientMMSCreateLobby>(EMsg.ClientMMSCreateLobby);
+            createlobbyRequest.Header.Proto.routing_appid = 730; // CS:GO APPID
+            createlobbyRequest.ProtoHeader.routing_appid = 730;
+            createlobbyRequest.Body.app_id = 730;
+            createlobbyRequest.Body.max_members = 10;
+            createlobbyRequest.Body.lobby_type = 1;
+            createlobbyRequest.Body.lobby_flags = 1;
+            createlobbyRequest.Body.cell_id = cellId;
+            createlobbyRequest.Body.persona_name_owner = Friends.GetPersonaName();
+            Client.Send(createRequest);
 
             Console.WriteLine("Created a lobby with 10 slots!");
         }
